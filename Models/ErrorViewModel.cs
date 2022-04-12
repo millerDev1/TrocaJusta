598988bc4981964713713c58f19e6049101adf17
Miller Souza <miller.souza@hotmail.com>
2022-04-12 10:40:56 -0300

    feat:creating project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrocaJusta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
