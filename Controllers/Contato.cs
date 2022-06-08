e8c2b4bb65ba9149f689d46a5bbfecd7d89db61b
Miller Souza <miller.souza@hotmail.com>
2022-06-08 10:41:34 -0300

    feat:atualizadinho

---
+++ Controllers/Contato.cs
@@ -0,0 +1,13 @@
+using System;
+using TrocaJusta.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace TrocaJusta.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }   
+}
