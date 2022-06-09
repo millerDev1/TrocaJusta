ba46224d6a8250020756b512a4c51023f61798f7
Miller Souza <miller.souza@hotmail.com>
2022-06-09 12:43:07 -0300

    feat:UPADETE2

---
+++ Controllers/Concluido.cs
@@ -0,0 +1,13 @@
+using System;
+using TrocaJusta.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace TrocaJusta.Controllers
+{
+    public class ConcluidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }   
+}
